412cdd9a875b4bc12673bf943748a169df2c4f3f
abu-bakarr <abangura14@yahoo.com>
2021-09-09 09:26:07 +0000

    eg 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    lis = dir(hidden_4)
-    new = []
-    for a in lis:
-        if a[0] != '_':
-            new.append(a)
-    new.sort()
-    for a in new:
-        print(a)
+import hidden_4
+dir(hidden_4)
