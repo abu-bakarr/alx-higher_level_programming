9ad873f2e3a1b7c21d16ea43409c7ed55cfccab2
abu-bakarr <abangura14@yahoo.com>
2021-09-13 09:18:22 +0000

    exercise advance

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#include <stdio.h>
+#include <Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+	int len;
+	int i;
+	PyObject *type;
+
+	if (!p)
+		return;
+	len = (int)PyList_Size(p);
+	i = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %d\n[*] Allocated = %d\n", len, i);
+	i = 0;
+	while (i < len)
+	{
+		type = PyList_GetItem(p, i);
+		printf("Element %d: %s\n", i, Py_TYPE(type)->tp_name);
+		i++;
+	}
+}
