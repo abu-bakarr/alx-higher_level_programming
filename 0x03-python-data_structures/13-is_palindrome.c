ece85a6b13b33729238ae2f91a9cf8d9dde4a38c
abu-bakarr <abangura14@yahoo.com>
2021-09-13 09:07:10 +0000

    exercise 12

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+int is_palindrome(listint_t **head)
+{
+int len = 0, i, j, array[3000];
+if (head == NULL || *head == NULL || (*head)->next == NULL)
+{
+return (1);
+}
+while (*head != NULL)
+{
+array[len] = (*head)->n;
+*head = (*head)->next;
+len++;
+}
+for (i = 0, j = len - 1; i < len / 2 + 1; i++, j--)
+{
+if (array[i] != array[j])
+{
+return (0);
+}
+}
+return (1);
+}
