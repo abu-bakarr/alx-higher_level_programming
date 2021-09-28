cd2b04f26b6aaa19fb7af6ed0758c05ff0322a35
abu-bakarr <abangura14@yahoo.com>
2021-09-28 12:03:59 +0000

    eg 4

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+"""A empty class Square that defines a square
+This module provides a simple version of class
+"""
+
+
 class Square:
-    """ class square with function of size """
+    """The class definition"""
 
     def __init__(self, size=0):
-        if (type(size) is not int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif (size < 0):
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
         return self.__size ** 2
