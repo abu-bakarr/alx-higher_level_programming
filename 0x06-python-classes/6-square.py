e7aa6688a5a5df625ea5a7349ce21d00472f9bd9
abu-bakarr <abangura14@yahoo.com>
2021-09-28 12:06:35 +0000

    eg 7

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,47 +1,56 @@
 #!/usr/bin/python3
-class Square:
-    """ class square with function of size private attribute """
+"""A empty class Square that defines a square
+This module provides a simple version of class
+"""
 
+
+class Square:
+    """The class definition"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
+        """init"""
+        self.size = size
         self.position = position
 
     @property
     def size(self):
+        """size"""
         return self.__size
 
     @size.setter
-    def size(self, size=0):
-        if (type(size) is not int):
+    def size(self, value):
+        """set the size"""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif (size < 0):
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     @property
     def position(self):
+        """position"""
         return self.__position
 
     @position.setter
-    def position(self, size=0):
-        if (type(size) is not tuple or len(size) is not 2 or
-            type(size[0]) is not int or type(size[1]) is not int or
-                size[0] < 0 or size[1] < 0):
+    def position(self, value):
+        """set the size"""
+        if type(value) is not tuple or len(value) < 2 or\
+           (isinstance(value[1], int) is not True) or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = size
+            self.__position = value
 
     def area(self):
+        """calculate the area"""
         return self.__size ** 2
 
     def my_print(self):
-        if self.__size != 0:
-            for i in range(self.__position[1]):
-                print("")
-            for i in range(self.__size):
-                if self.__position[0]:
-                    print(" " * self.__position[0], end="")
-                print("#" * self.__size)
-        else:
-            print("")
+        """print the area"""
+        if self.__size == 0:
+            print()
+            return
+        for j in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            print(' ' * self.__position[0], end='')
+            print('#' * self.__size)
