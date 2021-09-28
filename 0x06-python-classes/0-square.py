236629cc174bc935370d30a00babf8c6140cd1c4
abu-bakarr <abangura14@yahoo.com>
2021-09-28 12:00:03 +0000

    eg 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""A empty class Square that defines a square
+This module provides a simple version of class
+"""
+
+
 class Square:
-    square = 0
+    """The class definition"""
+
+    pass
