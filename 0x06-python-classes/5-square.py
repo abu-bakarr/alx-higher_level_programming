2c5363c7cf67d27d582b3ddf4409471067be3d7c
abu-bakarr <abangura14@yahoo.com>
2021-09-28 12:05:44 +0000

    eg 6

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
-class Square:
-    """ class square with function of size private attribute """
+"""A empty class Square that defines a square
+This module provides a simple version of class
+"""
 
+
+class Square:
+    """The class definition"""
     def __init__(self, size=0):
-        self.__size = size
+        """init"""
+        self.size = size
 
     @property
     def size(self):
+        """size"""
         return self.__size
 
     @size.setter
-    def size(self, size=0):
-        if (type(size) is not int):
+    def size(self, value):
+        """set the size"""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif (size < 0):
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
+        """calculate the area"""
         return self.__size ** 2
 
     def my_print(self):
-        if self.__size != 0:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
-                print('')
-        else:
-            print("")
+        """print the area"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print('#' * self.__size)
