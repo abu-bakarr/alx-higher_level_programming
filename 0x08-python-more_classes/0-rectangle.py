15bd5a5ea6ee040ba9b391974b0b8e74e1669764
abu-bakarr <abangura14@yahoo.com>
2021-10-04 20:35:09 +0000

    1 exercise

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" this is an empty class that defines a rectangle """
+"""Doc
+"""
 
-class Rectangle():
-                """ docstring for Rentangle """
-                pass
+
+class Rectangle:
+    """ Doc """
+    pass
