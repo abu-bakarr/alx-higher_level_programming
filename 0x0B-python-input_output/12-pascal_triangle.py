0be0387cc62ee390dc651240bd22a341c5dbb345
abu-bakarr <abangura14@yahoo.com>
2021-10-12 12:26:25 +0000

    12 exercise

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """rReturns a list of lists of integers
+    representing the Pascal’s triangle of n"""
+    res = []
+    if n <= 0:
+        return []
+    for x in range(1, n + 1):
+        c = 1
+        res.append([])
+        for y in range(1, x + 1):
+            res[x - 1].append(c)
+            c = int(c * (x - y) / y)
+    return res
