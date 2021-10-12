d8b1db090ef0480a3d7085020445cdf38234f6cb
abu-bakarr <abangura14@yahoo.com>
2021-10-12 12:15:18 +0000

    4 exercise

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to_json_string"""
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON
+    representation of an object"""
+    return json.dumps(my_obj)
