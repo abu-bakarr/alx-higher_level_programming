855b177085a6812c29cfde2b8c86bbb4ed433341
abu-bakarr <abangura14@yahoo.com>
2021-11-17 21:43:32 +0000

    add ex adv

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- lists all shows from hbtn_0d_tvshows_rate by their rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating'
+FROM tv_show_ratings
+LEFT JOIN tv_shows
+ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
