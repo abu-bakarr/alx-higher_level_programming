d4ea1166f14e34e6ad8a09907792fce07a84c0c8
abu-bakarr <abangura14@yahoo.com>
2021-12-20 19:04:12 +0000

    my get

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""SQLalchemy"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (func, create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+
+
+if __name__ == "__main__":
+    db = {'drivername': 'mysql+mysqldb',
+          'host': 'localhost',
+          'port': '3306',
+          'username': sys.argv[1],
+          'password': sys.argv[2],
+          'database': sys.argv[3]}
+    url = URL(**db)
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    try:
+        data = session.query(State).filter(State.name == sys.argv[4]).first()
+        print(data.id)
+    except:
+        print("Not found")
+    session.close()
