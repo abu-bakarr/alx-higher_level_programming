894dd49920a71472fb3af4007990a46de7a1065b
abu-bakarr <abangura14@yahoo.com>
2021-12-20 19:02:49 +0000

    filter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""SQLalchemy"""
-import sys
-from model_state import Base, State
+"""
+lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
+"""
+from sys import argv
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
-from sqlalchemy.engine.url import URL
-
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    db = {'drivername': 'mysql+mysqldb',
-          'host': 'localhost',
-          'port': '3306',
-          'username': sys.argv[1],
-          'password': sys.argv[2],
-          'database': sys.argv[3]}
-    url = URL(**db)
-    engine = create_engine(url, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-
-    try:
-        data = session.query(State).filter(State.name.like('%a%'))
-        for state in data:
+    # Create engine. The args comes from input
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    # Start Session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # Query
+    states = session.query(State).order_by(State.id)
+    # Print results
+    for state in states:
+        if 'a' in state.name:
             print("{}: {}".format(state.id, state.name))
-    except:
-        print("Nothing")
-    session.close()
