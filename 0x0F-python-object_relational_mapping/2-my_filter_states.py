96d5eabe1790e816b6dafb3b175fafb7abf35b7c
abu-bakarr <abangura14@yahoo.com>
2021-12-20 17:01:19 +0000

    exercise 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,13 +3,12 @@
 import MySQLdb
 import sys
 
-
 if __name__ == '__main__':
     database = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], database=sys.argv[3])
-    current = database.cursor()
-    current.execute("SELECT * FROM states WHERE BINARY\
+                         password=sys.argv[2], database=sys.argv[3])
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY\
                 name='{:s}' ORDER BY id ASC".format(sys.argv[4]))
-    result = current.fetchall()
+    result = cursor.fetchall()
     for row in result:
         print(row)
