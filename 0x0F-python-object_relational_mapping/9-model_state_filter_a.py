6930a74c5d0deade4f5f96d53501dc49dd76dd16
abu-bakarr <abangura14@yahoo.com>
2021-12-20 18:56:04 +0000

    filter

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""SQLalchemy"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+
+
+if __name__ == "__main__":
+    db = {'drivername': 'mysql+mysqldb',
+          'host': 'localhost',
+          'port': '3306',
+          'username': sys.argv[1],
+          'password': sys.argv[2],
+          'database': sys.argv[3]}
+    url = URL(**db)
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    try:
+        data = session.query(State).filter(State.name.like('%a%'))
+        for state in data:
+            print("{}: {}".format(state.id, state.name))
+    except:
+        print("Nothing")
+    session.close()
