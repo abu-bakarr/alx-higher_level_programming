3dc520aab4e3184b94353e93e9236c0d17a9b2ba
abu-bakarr <abangura14@yahoo.com>
2022-01-10 15:53:14 +0000

    Add ex 7

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import requests
+""" Fetches https://intranet.hbtn.io/status """
+from requests import post
 from sys import argv
 
 
 if __name__ == "__main__":
-    print((requests.post(argv[1], data={'email': argv[2]})).text)
+    res = post(argv[1], {'email': argv[2]})
+    print(res.text)
