28fe42eed1446e9ca000ec3604979d49fb9c750f
abu-bakarr <abangura14@yahoo.com>
2022-01-10 15:39:52 +0000

    Add ex 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import urllib.request
+""" Fetches https://intranet.hbtn.io/status """
+import urllib.request as req
 import sys
+
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        html = response.headers['X-Request-Id']
-        print(html)
+    rr = req.Request(sys.argv[1])
+    with req.urlopen(rr) as res:
+        print(res.getheader('X-Request-Id'))
