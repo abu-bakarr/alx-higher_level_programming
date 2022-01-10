e6855804efaf708854e26e8ec17067f37f5910e7
abu-bakarr <abangura14@yahoo.com>
2022-01-10 15:37:35 +0000

    Add ex 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+""" Fetching URLs with urllib """
 import urllib.request
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print('Body response:')
+        print('\t- type: ', end='')
+        print(type(html))
+        print('    - content: ', end='')
+        print(html)
+        print('\t- utf8 content: ', end='')
+        print(html.decode('utf-8'))
