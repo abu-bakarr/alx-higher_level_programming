13ded88d99443f54b233fc7c8b280b30acdb31e6
abu-bakarr <abangura14@yahoo.com>
2022-01-10 15:34:43 +0000

    Add ex 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" Fetches https://intranet.hbtn.io/status """
-import urllib.request as req
-
+"""
+    Python script that fetches a URL
+"""
+import urllib.request
 
 if __name__ == "__main__":
-    rr = req.Request('https://intranet.hbtn.io/status')
-    with req.urlopen(rr) as res:
-        body = res.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(body)))
-        print('\t- content: {}'.format(body))
-        print('\t- utf8 content: {}'.format(body.decode("utf-8")))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        html2 = html.decode("utf-8")
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html2))
