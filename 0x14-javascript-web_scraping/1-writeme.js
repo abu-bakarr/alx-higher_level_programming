3369337c6c220ecc5ebe054cee79508f8fc01cf1
abu-bakarr <abangura14@yahoo.com>
2022-02-01 10:21:08 +0000

    Add exer 2

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filename = process.argv[2];
+const textContent = process.argv[3];
+
+fs.writeFile(filename, textContent, 'utf8', (err) => {
+    if (err) {
+        console.log(err);
+    }
+});
