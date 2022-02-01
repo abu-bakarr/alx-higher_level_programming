2ea63b176fc2e852dafd671753072f486526385a
abu-bakarr <abangura14@yahoo.com>
2022-02-01 10:48:43 +0000

    Add file exer adv

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,14 +8,14 @@ const peopleId = args[2];
 const apiUrl = `https://swapi-api.hbtn.io/api/people/${peopleId}`;
 const options = { json: true };
 
-request(apiUrl, options, (error, response, body) => {
+request(apiUrl, options, function(error, response, body) {
     if (error) {
         console.error(error);
         return;
     }
     const casting = body.characters;
     for (const cast in casting) {
-        request(casting[cast], options, (error, response, body) => {
+        request(casting[cast], options, function(error, response, body) {
             if (error) {
                 console.error(error);
             } else {
